d09f2426ae879e3baa88941026bcc297d1c9dc04
razzkumar <razzkumar.dev@gmail.com>
2024-12-13 09:30:07 +0545

    feat: nvim update avante

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -5,8 +5,18 @@ return {
   version = false, -- set this if you want to always pull the latest change
   opts = {
     -- add any opts here
-    provider = "claude",
+    -- provider = "claude",
+    provider = "gemini",
     -- auto_suggestions_provider = "gemini",
+    -- claude = {
+    --   endpoint = "https://api.anthropic.com",
+    --   model = "claude-3-5-sonnet-20241022",
+    --   temperature = 0,
+    --   max_tokens = 4096,
+    -- },
+    gemini = {
+      model = "gemini-exp-1206",
+    },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
