f08f17a8a47ba8665d5d6e1d0b7d3be30cde783d
razzkumar <razzkumar.dev@gmail.com>
2025-01-17 08:38:56 +0545

    feat: setup aider

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,12 @@
 --
 
 vim.opt.laststatus = 3
+vim.g.snacks_animate = false
+
+if vim.g.neovide then
+  vim.g.neovide_transparency = 0.8
+  vim.g.neovide_normal_opacity = 0.8
+  vim.g.neovide_show_border = false
+  -- vim.g.neovide_fullscreen = true
+  -- vim.g.neovide_window_blurred = true
+end
