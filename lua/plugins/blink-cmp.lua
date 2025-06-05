f01c42b2aa3b6a7c7f228d362f14e02bbd1f2360
razzkumar <razzkumar.dev@gmail.com>
2025-06-05 08:26:32 +0545

    feat: setup lazygit

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -1,10 +1,13 @@
 return {
     "saghen/blink.cmp",
     event = "LspAttach",
-    build = "...",
     version = "1.*",
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
+    build = "cargo build --release",
+
+    dependencies = {
+        "rafamadriz/friendly-snippets",
+    },
+
     opts = {
         cmdline = { enabled = false },
         fuzzy = {
@@ -22,11 +25,32 @@ return {
                 auto_show = true,
                 window = { border = "rounded" },
             },
-            menu = { border = "rounded" },
+            menu = {
+                border = "rounded",
+            },
         },
+
         appearance = {
             nerd_font_variant = "normal",
             kind_icons = require("utils.kinds").icons,
+        },
+
+        keymap = {
+            preset = "enter",
+            ["<Tab>"] = { "select_next", "fallback" },
+            ["<S-Tab>"] = { "select_prev", "fallback" },
+            ["<C-y>"] = { "select_and_accept" },
+            ["<C-e>"] = { "cancel", "fallback" },
+            ["<CR>"] = {
+                function()
+                    local cmp_popup = require("blink.cmp").popup
+                    if cmp_popup and cmp_popup:is_visible() then
+                        cmp_popup:cancel()
+                    else
+                        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<CR>", true, false, true), "n", false)
+                    end
+                end,
+            },
         },
     },
 }
